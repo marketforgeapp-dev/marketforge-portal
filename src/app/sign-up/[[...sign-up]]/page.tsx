22a9342da0be56917993c4ed055423e87e7f5c99
Patrick Donovan <patrick.donovan50@gmail.com>
2026-03-19 19:33:17 -0400

    Set explicit Clerk auth routes

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,13 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <SignUp
+      routing="path"
+      path="/sign-up"
+      signInUrl="/sign-in"
+      forceRedirectUrl="/portal/onboarding"
+      fallbackRedirectUrl="/portal/onboarding"
+    />
+  );
 }
