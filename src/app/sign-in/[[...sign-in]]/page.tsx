88dc9afa7dd366db82b90cce92bd7be3a11eb827
Patrick Donovan <patrick.donovan50@gmail.com>
2026-03-19 16:24:21 -0400

    Trying to solve Clerk errors

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,19 +1,5 @@
-"use client";
-
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return (
-    <main className="min-h-screen bg-[#081018] px-6 py-16 text-white">
-      <div className="mx-auto flex max-w-6xl items-center justify-center">
-        <SignIn
-          routing="path"
-          path="/sign-in"
-          signUpUrl="/sign-up"
-          forceRedirectUrl="/portal/dashboard"
-          fallbackRedirectUrl="/portal/dashboard"
-        />
-      </div>
-    </main>
-  );
+  return <SignIn />;
 }
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,19 +1,5 @@
-"use client";
-
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return (
-    <main className="min-h-screen bg-[#081018] px-6 py-16 text-white">
-      <div className="mx-auto flex max-w-6xl items-center justify-center">
-        <SignUp
-          routing="path"
-          path="/sign-up"
-          signInUrl="/sign-in"
-          forceRedirectUrl="/portal/onboarding"
-          fallbackRedirectUrl="/portal/onboarding"
-        />
-      </div>
-    </main>
-  );
+  return <SignUp />;
 }
